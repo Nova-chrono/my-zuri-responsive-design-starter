be4b08e7008bde46a5216386d01c14518899ed0f
Novachrono <prosperjoechinedum@gmail.com>
2021-05-04 22:25:45 +0100

    first responsive design commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const navLinks = document.querySelector('.nav-links');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+
+function show() {
+    navLinks.classList.remove('hide');
+    navLinks.classList.add('show');
+    closeMenu.style.display= 'block'
+    openMenu.style.display = 'none'
+}
+
+function close() {
+    navLinks.classList.remove('show');
+    navLinks.classList.add('hide');
+    closeMenu.style.display= 'none'
+    openMenu.style.display = 'block'
+}
